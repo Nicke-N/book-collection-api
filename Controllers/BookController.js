176985b0ea817748ed5e5aa244aaea878ae01f3e
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-04 13:59:23 +0200

    Changed layout in controller and model

--- Controllers/BookController.js
+++ Controllers/BookController.js
@@ -5,7 +5,7 @@ module.exports = {
     async getCollection (req, res, next) {
 
         try {
-            const collection = await books.find({})
+            const collection = await books.getCollection()
             res.json(collection)
         } catch (error) {
             next(error)
@@ -17,7 +17,7 @@ module.exports = {
 
         try {
             
-            const book = await books.findOne({_id: id})
+            const book = await books.getBook(id)
             res.json(book)
 
         } catch (error) {
@@ -28,17 +28,9 @@ module.exports = {
     async addBook (req, res, next) {
 
         try {
-            const book = new books(req.body)
-            const bookExists = await checkDB(req.body)
-
-            if (!bookExists) {
-                book.save(err => {
-                    if (err) return res.status(500).send(err)
-                    return res.send(`${book.title} was added`)
-                })
-            } else {
-                res.send('Book exists!')
-            }
+            
+            await books.addBook(req.body)
+            res.status(200).send('Book was added!')
 
         } catch (error) {
             next(error)
@@ -50,14 +42,8 @@ module.exports = {
 
         try {
             
-            books.findByIdAndUpdate(
-                id, 
-                req.body,
-                (err, book) => {
-                    if (err) return res.status(500).send(err)
-                   
-                    return res.send(book)
-            })
+            await books.editBook(id, req.body)
+            res.status(200).send('Book was updated!')
 
         } catch (error) {
             next(error)
@@ -69,13 +55,8 @@ module.exports = {
         const id = req.params.bookID
         try {
             
-            books.findByIdAndRemove(
-                id, 
-                (err, book) => {
-                    if (err) return res.status(500).send(err)
-
-                    return res.status(200).send({msg: `${book.title} was deleted!`})
-            })
+            await books.deleteBook(id)
+            res.status(200).send('Book was deleted!')
 
         } catch (error) {
             next(error)
@@ -86,27 +67,3 @@ module.exports = {
 }
 
 
-const checkDB = async (book) => {
-
-    try {
-        const existingBooks = await books.find({title: book.title})
-        var result = false
-        if (existingBooks.length > 0) {
-
-            existingBooks.map((element) => {
-
-                (element.title === book.title && element.author === book.author) ? result = true : null
-                
-                if (result) return result
-            })
-
-        }
-
-        return result
-
-    } catch (error) {
-        return error
-    }
-
-
-}
